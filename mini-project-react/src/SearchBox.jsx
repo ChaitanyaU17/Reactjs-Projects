f84c83eea64246ff423d46a0b634f89f9f2485b8
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-06-27 19:31:42 +0530

    handle error

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -3,61 +3,71 @@ import classes from './SearchBar.module.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { API_URL, API_KEY } from './utils/constants';
+import Alert from '@mui/material/Alert';
 
-const SearchBox = ( { updateInfo } ) => {
-    const[city, setCity] = useState('');
-
-   
+const SearchBox = ({ updateInfo }) => {
+    const [city, setCity] = useState('');
+    const [error, setError] = useState(false);
 
     const getWeatherData = async () => {
         const response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
         const data = await response.json();
-        //console.log(data);
 
-        let result = {
-            city: city,
-            temp: data.main.temp,
-            tempMax: data.main.temp_max,
-            tempMin: data.main.temp_min,
-            humidity: data.main.humidity,
-            feels_like: data.main.feels_like,
-            weather: data.weather[0].description,
+        if (response.ok) {
+            let result = {
+                city: city,
+                temp: data.main.temp,
+                tempMax: data.main.temp_max,
+                tempMin: data.main.temp_min,
+                humidity: data.main.humidity,
+                feels_like: data.main.feels_like,
+                weather: data.weather[0].description,
+            };
+            console.log(result);
+            return result;
+        } else {
+            throw new Error('City not found');
         }
-        console.log(result);
-        return result;
-
-    }
+    };
 
     const handleInput = (event) => {
         setCity(event.target.value);
-    }
+    };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log(city);
-        setCity('');
-        const newInfo = await getWeatherData();
-        updateInfo(newInfo);
-    }
+        try {
+            console.log(city);
+            setCity('');
+            const newInfo = await getWeatherData();
+            updateInfo(newInfo);
+            setError(false);
+        } catch (err) {
+            setError(true);
+        }
+    };
 
     return (
-      <div className={classes.searchbar}>
-        <form onClick={handleSubmit}>
-        <TextField 
-        id="outlined-basic" 
-        label="city name" 
-        variant="outlined" 
-        value={city}
-        onChange={handleInput}
-        />
-        <p>
-          <Button className="search-btn" variant="contained" type='submit'>
-            Search
-          </Button>
-        </p>
-        </form>
-      </div>
+        <div className={classes.searchbar}>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    id="outlined-basic"
+                    label="City Name"
+                    variant="outlined"
+                    value={city}
+                    onChange={handleInput}
+                />
+                <p>
+                    <Button className="search-btn" variant="contained" type="submit">
+                        Search
+                    </Button>
+                </p>
+            </form>
+            <div className={classes.error}>
+            {error && <Alert severity="error">No such a place exist in API!</Alert>}
+            </div>
+        </div>
     );
-}
+};
 
 export default SearchBox;
