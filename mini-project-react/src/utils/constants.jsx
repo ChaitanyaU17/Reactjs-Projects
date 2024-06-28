ae1cd0c844e606a08eb18ae478276b329f3c5237
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-06-28 09:23:12 +0530

    weather app

--- mini-project-react/src/utils/constants.jsx
+++ mini-project-react/src/utils/constants.jsx
@@ -3,3 +3,9 @@ export const API_KEY = '31a17b991af08bd45fd1adca2daf30b9';
 export const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export  const IMG_HAZE = 'https://images.unsplash.com/photo-1519944159858-806d435dc86b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhhemUlMjB3ZWF0aGVyfGVufDB8fDB8fHww';
+
+export const HOT_IMG = 'https://images.unsplash.com/photo-1615752592676-f6bd84f9419d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHN1bmxpZ2h0fGVufDB8fDB8fHww';
+ 
+export const COLD_IMG = 'https://images.unsplash.com/photo-1612208695882-02f2322b7fee?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D';
+
+export const RAINY_IMG = 'https://images.unsplash.com/photo-1519692933481-e162a57d6721?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmFpbnxlbnwwfHwwfHx8MA%3D%3D';
