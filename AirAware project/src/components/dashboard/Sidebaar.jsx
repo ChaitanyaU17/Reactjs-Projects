378bcfc68a2330fa5b10f5155a469cdcbe8da02f
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-25 21:02:36 +0530

    define side component

---
+++ AirAware project/src/components/dashboard/Sidebaar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+
+const Sidebaar = () => {
+    const [open, setOpen] = useState(false);
+
+    
+
+    return (
+        <>
+           
+        </>
+    );
+};
+
+export default Sidebaar;
