e669247689ec28fbd0e0359bcb7a7c2b4dd60ee4
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-25 20:49:34 +0530

    define header component with content of wheather patterns in array format display dropdown

---
+++ AirAware project/src/components/dashboard/education/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Language from "../../../json/language.json"
+const Header = ({ setTopics, setLang }) => {
+
+
+    const Content = [
+        "today weather update",
+        "Real-time weather update",
+        "Understanding Weather Patterns",
+        "How to Read Weather Forecasts",
+        "Effects of Extreme Weather",
+        "Seasonal Weather Changes",
+        "Extreme Weather Events",
+        "Weather vs. Climate",
+        "Air Pollution and Its Effects",
+        "Causes of Air Pollution",
+        "Water Pollution Causes and Solutions",
+        "Impact of Water Pollution",
+        "Soil Pollution and Its Impact",
+        "Noise Pollution Control",
+        "Indoor Air Quality",
+        "Climate Change Explained",
+        "Impact of Climate Change",
+        "Global Warming and Its Effects",
+        "Sustainable Practices to Combat Climate Change",
+        "Renewable Energy Solutions",
+        "Carbon Footprint Reduction",
+        "Climate Change and Natural Disasters",
+        "Understanding Greenhouse Gases",
+        "Mitigating Climate Change Effects",
+        "Climate Policy and Legislation",
+        "Adaptation Strategies for Climate Change",
+        "Climate Change and Agriculture",
+        "Urban Heat Islands and Their Effects"
+    ]
+
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Header
