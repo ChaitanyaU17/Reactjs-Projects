8193373b639819edea86c0d84bcb590a4d6b64b8
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-26 22:34:22 +0530

    add wheather data in form of card

--- AirAware project/src/components/dashboard/weather/WeatherData.jsx
+++ AirAware project/src/components/dashboard/weather/WeatherData.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { FaCloud, FaEye, FaTemperatureHigh, FaWind } from 'react-icons/fa';
+import { FaTachometerAlt } from 'react-icons/fa';//Pressure
+import { FaTint } from 'react-icons/fa';//Humidity
+import { GiSunrise } from 'react-icons/gi';
+import { IoSunny } from 'react-icons/io5';
+import { TiCompass } from 'react-icons/ti';
+// import { MdStorm } from 'react-icons/md'; // Use MdStorm for a stormy weather icon
 import { useSelector } from 'react-redux';
 /* rechart */
 
@@ -23,9 +30,39 @@ const WeatherData = () => {
         return time;
     }
 
+    const sunrise = weatherData?.sys?.sunrise;
+    const sunset = weatherData?.sys?.sunset;
+
+    const sunriseTime = convertUnixToTime(sunrise);
+    const sunsetTime = convertUnixToTime(sunset);
+
+
+    // console.log(weatherData?.weather[0]?.description)
 
     return (
         <>
+            {/* Weather Cards */}
+            <div className='w-full h-auto flex  flex-col xl:flex-row lg:flex-row md:flex-row sm:flex-col justify-between p-4 gap-4'>
+            
+                {/* card 1 */}
+                <div className='w-full md:w-[40%] h-[200px] rounded-md bg-red-100 shadow-sm flex'>
+                    <div className='w-[150px] h-full flex flex-col justify-center items-center p-4 '>
+                        <img src={`http://openweathermap.org/img/wn/${weatherData?.weather && weatherData?.weather[0]?.icon}@2x.png`} alt='weather' className='w-[100px] h-[100px]' />
+                        <p className='text-xl font-bold '>{weatherData?.weather && weatherData?.weather[0]?.main}</p>
+                        <p className='text-base'>{weatherData?.weather && weatherData?.weather[0]?.description}</p>
+                    </div>
+                    <div className='w-[60%] h-auto flex flex-col gap-4 justify-center items-center'>
+                        <h1 className='text-7xl  font-bold'>{kelvinToCelsius(weatherData?.main?.temp).toFixed(0)}°C</h1>
+
+                        <p >max: {kelvinToCelsius(weatherData?.main?.temp_min).toFixed(0)}°C  min: {kelvinToCelsius(weatherData?.main?.temp_max).toFixed(0)}°C</p>
+                    </div>
+                </div>
+
+                
+            
+            </div>
+
+
         </>
     );
 }
