f80ae53b11632c29c6859e5bf6bb9b0f83adeb4f
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-26 17:12:38 +0530

    dispatch wheather data from store

--- AirAware project/src/components/dashboard/Pollustion/Aqi.jsx
+++ AirAware project/src/components/dashboard/Pollustion/Aqi.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-
+import { FaCloud, FaSmog, FaWind, FaSun, FaCloudSun, FaCloudMoon, FaLeaf } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 
 const Aqi = ({ datauserprotectaqi }) => {
-   
+    const pollustiondata = useSelector((state) => state.pollustion.data);
+    const weatherData = useSelector((state) => state.weather.data);
+    const WeatherCityname = weatherData?.name;
+    // console.log(pollustiondata?.list && pollustiondata?.list[0]?.components)
+
+
+    const datapollution = pollustiondata?.list && pollustiondata?.list[0]?.components;
+
 
 
     return (
         <>
-        
+           
         </>
     )
 }
