61d0e2f7c39d73428a3ba76422dfdf0218d187ae
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-26 16:51:52 +0530

    in dashboard component composition of sidebar

---
+++ AirAware project/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebaar from '../components/dashboard/Sidebaar';
+
+const Dashboard = () => {
+    return (
+        <div className='w-full h-[100vh] flex'>
+            <Sidebaar />
+        </div>
+    );
+}
+
+export default Dashboard;
