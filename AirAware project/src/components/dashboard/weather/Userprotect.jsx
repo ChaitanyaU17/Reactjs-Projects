4cf713403e1ba6a6b7f9d250e8033f1df7694d99
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-26 22:28:40 +0530

    day and night protection and current time

--- AirAware project/src/components/dashboard/weather/Userprotect.jsx
+++ AirAware project/src/components/dashboard/weather/Userprotect.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { FaSnowflake, FaInfoCircle, FaExclamationTriangle, FaHeart, FaFirstAid } from 'react-icons/fa';
+import { FaSun, FaMoon } from 'react-icons/fa';
 import ProtectJsonData from "../../../json/TempData.json"
 import { useSelector } from 'react-redux';
 const Userprotect = () => {
     // console.log('ProtectJsonData', ProtectJsonData)
-
+    const [datauserprotect, setDatauserprotect] = useState([''])
+    const [daynighttimeprotection, setDaynighttimeprotection] = useState(true)
     const kelvinToCelsius = (kelvin) => kelvin - 273.15;
 
     const weatherData = useSelector((state) => state.weather.data);
- 
+    // const weatherStatus = useSelector((state) => state.weather.status);
+    // const weatherError = useSelector((state) => state.weather.error);
 
     const temp = kelvinToCelsius(weatherData?.main?.temp).toFixed(0);
 
@@ -48,8 +52,44 @@ const Userprotect = () => {
     // Determine if it's day or night
     const isDaytime = currentTime >= sunrise && currentTime < sunset;
 
+    /* auto userprotection day and night  */
+    useEffect(() => {
+        if (isDaytime) {
+            setDaynighttimeprotection(true)
+        } else {
+            setDaynighttimeprotection(false)
+        }
+    }, [isDaytime])
+
+    const dayprotection = () => {
+        setDaynighttimeprotection(true)
+    }
+
+    const nightprotection = () => {
+        setDaynighttimeprotection(false)
+    }
+
+
     return (
-        <></>
+        <div className="container mx-auto p-4 bg-gray-800 text-white">
+            <div className='w-full h-auto flex gap-4 py-2'>
+                <button
+                    onClick={dayprotection}
+                    className={`w-[100px] h-[40px] flex justify-center items-center rounded-md cursor-pointer ${daynighttimeprotection ? 'bg-white text-black' : 'border'}`}
+                    title='Day'
+                >
+                    <FaSun className='mr-2 text-yellow-400' /> Day
+                </button>
+                <button
+                    onClick={nightprotection}
+                    className={`w-[100px] h-[40px] flex justify-center items-center rounded-md cursor-pointer ${!daynighttimeprotection ? 'bg-white text-black border' : 'border'}`}
+                    title='Night'
+                >
+                    <FaMoon className='mr-2 text-blue-400' /> Night
+                </button>
+            </div>
+            
+        </div>
     );
 };
 
