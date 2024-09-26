2fc83461537b33249519b5c235af42ec5ba0e066
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-26 16:46:03 +0530

    initialize hero component contains navbar, hero and footer component composition

---
+++ AirAware project/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from '../components/layout/Nav';
+import Hero from '../components/home/Hero';
+import Footer from '../components/layout/Footer';
+const Home = () => {
+    return (
+        <>
+            <Nav />
+            <Hero />
+            <Footer/>
+        </>
+    )
+}
+
+export default Home;
