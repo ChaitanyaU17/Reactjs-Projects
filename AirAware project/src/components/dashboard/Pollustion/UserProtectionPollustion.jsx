d416102259718adef600865ea94b2466eee2aa1a
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-26 22:06:22 +0530

    render health recommendations

--- AirAware project/src/components/dashboard/Pollustion/UserProtectionPollustion.jsx
+++ AirAware project/src/components/dashboard/Pollustion/UserProtectionPollustion.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { FaInfoCircle, FaShieldAlt, FaChild, FaStethoscope, FaExclamationCircle, FaHandPaper, FaMapMarkerAlt, FaBell, FaBaby, FaUserMd, FaUserAlt, FaHeartBroken, FaRunning, FaCar, FaHome, FaHospital, FaBuilding } from 'react-icons/fa';
 
-const UserProtectionPollution = () => {
+const UserProtectionPollution = ({ datauserprotectaqi }) => {
+
 
     return (
-        <></>
-    )
+        <div className="container mx-auto p-4 text-gray-100">
+           
+
+                <h3 className="text-lg font-semibold mb-2 flex items-center">
+                    <FaStethoscope className="mr-2 text-teal-400 text-xl" />
+                    Health Recommendations:
+                </h3>
+                <ul className="list-disc pl-5 mb-4">
+                    {
+                        datauserprotectaqi?.health_recommendations?.map((health, index) => (
+                            <>
+                                <li key={index}>{health}</li>
+                            </>
+                        ))
+                    }
+
+
+                </ul>
+
+                
+
+                
+       </div>
+    );
 };
 
 export default UserProtectionPollution;
