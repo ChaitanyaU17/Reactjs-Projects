5ec4937bb665cd7d64939691d201e03c169e8306
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-26 16:56:29 +0530

    render videodata map over array

--- AirAware project/src/components/dashboard/education/Lodder.jsx
+++ AirAware project/src/components/dashboard/education/Lodder.jsx
@@ -14,8 +14,16 @@ const Lodder = () => {
     ];
 
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
+            {videodata?.map((data, index) => (
+                <div
+                    key={index}
+                    className='w-full h-[250px] cursor-pointer rounded-md overflow-hidden bg-gray-200 shadow-sm animate-pulse'
+                >
+                    <div className='w-full h-[200px] bg-gray-300'></div>
+                    <p className='w-full text-[14px] p-2 bg-gray-300 h-4 '></p>
+                </div>
+            ))}
         </div>
     );
 };
