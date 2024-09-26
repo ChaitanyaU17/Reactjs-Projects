5aae7f2c14cb771fc397f92bb305cfa0fc380566
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-26 22:16:06 +0530

    dispatch wheather data get unique dates loading map

--- AirAware project/src/components/dashboard/weather/Map_Forcast.jsx
+++ AirAware project/src/components/dashboard/weather/Map_Forcast.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Markermap from '../../../images/marker.png';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { useSelector } from 'react-redux';
 
 delete L.Icon.Default.prototype._getIconUrl;
 const customIcon = new L.Icon({
@@ -12,8 +14,55 @@ const customIcon = new L.Icon({
 });
 
 const Map_Forcast = () => {
-   return (
-    <></>
+    const weatherData = useSelector((state) => state.weather.data);
+    /* weather forcast */
+    const weatherforcast = useSelector((state) => state.weatherforcast.data);
+
+    let position = null;
+    if (weatherData?.coord?.lat && weatherData?.coord?.lon) {
+        position = [weatherData.coord.lat, weatherData.coord.lon];
+    }
+
+    // Extract unique dates and their forecast data
+    const getUniqueDates = (data) => {
+        if (!data || !data.list) return [];
+        const dates = data.list.map(item => item.dt_txt.split(' ')[0]);
+        return [...new Set(dates)];
+    };
+
+    const aggregateDataByDate = (data, dates) => {
+        if (!data || !data.list) return {};
+        return dates.reduce((acc, date) => {
+            acc[date] = data.list.filter(item => item.dt_txt.startsWith(date));
+            return acc;
+        }, {});
+    };
+
+    const uniqueDates = getUniqueDates(weatherforcast);
+    const aggregatedData = aggregateDataByDate(weatherforcast, uniqueDates);
+
+
+    return (
+        <div className='w-full h-auto flex flex-col lg:flex-row gap-6 p-6'>
+            {/* Map Section */}
+            <div className='w-full lg:w-1/3 h-[350px] rounded-md overflow-hidden shadow-sm  '>
+                {position ? (
+                    <MapContainer center={position} zoom={6} style={{ height: '350px', width: '100%' }}>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        />
+                        <Marker position={position} icon={customIcon}>
+                            <Popup>A sample popup message!</Popup>
+                        </Marker>
+                    </MapContainer>
+                ) : (
+                    <div className="flex items-center justify-center h-full">Loading map...</div>
+                )}
+            </div>
+
+           
+        </div>
     );
 };
 
