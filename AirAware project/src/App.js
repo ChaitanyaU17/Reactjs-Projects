962b390f20e242a1b5d7fb6aa7cbe6cf54180544
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-27 09:26:55 +0530

    composite home and dashboard component with handle online off line status

--- AirAware project/src/App.js
+++ AirAware project/src/App.js
@@ -1,12 +1,47 @@
-import React from 'react'
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 
-const App = () => {
+function App() {
+  /* Check internet connection */
+  useEffect(() => {
+    const handleOffline = () => {
+      toast.error("You are offline");
+    };
+
+    const handleOnline = () => {
+      toast.success("You are online");
+    };
+
+    window.addEventListener("offline", handleOffline);
+    window.addEventListener("online", handleOnline);
+
+    // Cleanup function to remove event listeners
+    return () => {
+      window.removeEventListener("offline", handleOffline);
+      window.removeEventListener("online", handleOnline);
+    };
+  }, []);
+
+
+  
+
   return (
-    <div>
-      Hello Reactjs
-    </div>
-  )
+    <>
+    
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+          </Route>
+        </Routes>
+      </Router>
+      <Toaster />
+    </>
+  );
 }
 
-export default App
+export default App;
