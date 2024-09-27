621c16907c03c90599acd5677fb9200a4cf04b1e
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-27 10:37:56 +0530

    add outlet to dashboard component

--- AirAware project/src/pages/Dashboard.jsx
+++ AirAware project/src/pages/Dashboard.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Sidebaar from '../components/dashboard/Sidebaar';
+import { Outlet } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div className='w-full h-[100vh] flex'>
             <Sidebaar />
+            <div className='w-[100%] h-[100vh]  overflow-auto'>
+                <Outlet />
+
+            </div>
         </div>
     );
 }
