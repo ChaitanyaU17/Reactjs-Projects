cf48a58c4697e7f769c92e579744661526ad1d80
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-27 09:11:49 +0530

    disease card add health images

---
+++ AirAware project/src/components/home/DiseaseCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Asthma from "../../images/asthma.png"
+import Allergies from "../../images/hives.png"
+import Heart from '../../images/heart.png'
+import Heat from "../../images/sweating.png"
+import Dehydration from "../../images/sun.png"
+import Hypothermia from "../../images/cold.png"
+
+const DiseaseCard = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default DiseaseCard;
