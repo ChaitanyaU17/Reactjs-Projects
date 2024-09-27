07009f38b26fe914284747808ca035ca1206417d
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-27 10:30:48 +0530

    redux store wheather, forecast, input slices subscribed

--- AirAware project/src/store.js
+++ AirAware project/src/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import WeatherApiSlice from "./redux/WeatherApiSlice";
+import WeatherForcastSlice from "./redux/WeatherForcastSlice";
 import pollustionSlice from "./redux/Pollustion";
-
+import inputSlice from "./redux/Searchvalue";
 import locationSlice from "./redux/Permissions/location";
 import notificationsSlice from "./redux/notificationsSlice";
 export const store = configureStore({
   reducer: {
+    weather: WeatherApiSlice,
+    weatherforcast: WeatherForcastSlice,
     pollustion: pollustionSlice,
+    input: inputSlice,
     location: locationSlice,
     notifications:notificationsSlice
   },
