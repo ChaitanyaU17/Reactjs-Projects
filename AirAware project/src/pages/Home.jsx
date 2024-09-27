a5f7ba5ac3f9971f66496dec391848114f2485a0
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2024-09-27 10:34:57 +0530

    home component add impact and help section

--- AirAware project/src/pages/Home.jsx
+++ AirAware project/src/pages/Home.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import Nav from '../components/layout/Nav';
 import Hero from '../components/home/Hero';
+import DiseaseCard from '../components/home/DiseaseCard';
+import Feature from '../components/home/Feature';
+import ImpactSection from '../components/home/ImpactSection';
+import HelpSection from '../components/home/HelpSection';
 import Footer from '../components/layout/Footer';
 const Home = () => {
     return (
         <>
             <Nav />
             <Hero />
+            <DiseaseCard/>
+            <Feature/>
+            <ImpactSection/>
+            <HelpSection/>
             <Footer/>
         </>
     )
 }
 
-export default Home;
+export default Home
