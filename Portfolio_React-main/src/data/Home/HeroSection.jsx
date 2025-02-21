d25ed7226e1a5c3ec32a7325aa70872af7f16b17
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-02-21 22:14:50 +0530

    resume link updated

--- Portfolio_React-main/src/data/Home/HeroSection.jsx
+++ Portfolio_React-main/src/data/Home/HeroSection.jsx
@@ -33,7 +33,7 @@ export default function HeroSection() {
 
                 </div>
                 <button className="btn btn-github">
-                    <a  href="https://drive.google.com/file/d/17bZHLAyRNL1ajZwQWQz7tZRo4EPFluP_/view?usp=sharing">Resume</a>
+                    <a  href="https://drive.google.com/file/d/1dtxrraI3xpF_pv7mvuSk0p5ftpxeoaSG/view?usp=drive_link">Resume</a>
                 </button>
 
             </div>
